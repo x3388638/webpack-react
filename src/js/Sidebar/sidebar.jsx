fff92ae2c51a276639f8a396413105edf153ec80
YY <z3388638@gmail.com>
2016-04-17 03:09:25 +0800

    add Sidebar component

---
+++ src/js/Sidebar/sidebar.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import './sidebar';
+
+class Item extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div className="sidebar-item">{this.props.children}</div>
+		);
+	}
+}
+
+class Sidebar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			open: this.props.isOpen
+		};
+		this.handleOpenSidebar = this.handleOpenSidebar.bind(this);
+	}
+	componentWillReceiveProps(nextProps) {
+		if(nextProps.isOpen != this.props.isOpen) {
+			this.setState({
+				open: nextProps.isOpen
+			})
+		}
+	}
+	handleOpenSidebar() {
+		this.props.onToggle(!this.props.isOpen);
+	}
+	render() {
+		return (
+			<div id="sidebar" className={this.state.open ? 'open' : ''}>
+				<span className="glyphicon glyphicon-menu-hamburger btn-toggleSidebar" aria-hidden="true" onClick={this.handleOpenSidebar}></span>
+				<div className="content">
+					{this.props.children}
+				</div>
+			</div>
+		);
+	}
+}
+
+Sidebar.Item = Item;
+
+module.exports = Sidebar;
