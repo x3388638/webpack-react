7601f060a742bc567bb66d542188c85772885c67
YY <z3388638@gmail.com>
2016-04-17 04:05:52 +0800

    fix sidebar css & add sidebar usage

--- src/js/sidebarWrap.jsx
+++ src/js/sidebarWrap.jsx
@@ -17,14 +17,56 @@ class SidebarWrap extends React.Component {
 	}
 	render() {
 		return (
-			<div className="liveView sidebarWrap">
+			<div className="sidebarWrap">
 				<Sidebar isOpen={this.state.sidebarOpen} onToggle={this.handleToggleSidebar}>
 					<Sidebar.Item>Sidebar.Item 01</Sidebar.Item>
 					<Sidebar.Item>Sidebar.Item 02</Sidebar.Item>
 					<Sidebar.Item>Sidebar.Item 03</Sidebar.Item>
 				</Sidebar>
 				<div className={ `outerWrap ${this.state.sidebarOpen ? 'open' : ''}` } > 
-					<h2>outerWrap</h2>
+					<h2>Needed</h2>
+					<ol>
+						<li>
+							Import
+							<pre><code>
+							## import Sidebar from '/PATH/TO/Sidebar/sidebar';
+							</code></pre>
+						</li>
+						<li>
+							Structure
+							<pre><code>
+							## &lt;div className=".sidebarWrap" &gt;<br />
+							##     &lt;Sidebar&gt;<br />
+							##         &lt;Sidebar.Item&gt;<br />
+							##             sidebar item<br />
+							##         &lt;/Sidebar.Item&gt;<br />
+							##     &lt;/Sidebar&gt;<br />
+							##     &lt;div className=".outerWrap" &gt;<br />
+							##         OuterWrap<br />
+							##     &lt;/div&gt;<br />
+							## &lt;/div&gt;
+							</code></pre>
+						</li>
+						<li>
+							Handler
+							<pre><code>
+							## &lt;Sidebar isOpen={"{this.state.sidebarOpen}"} onToggle={"{this.handleToggleSidebar}"}&gt;<br />
+							## <br />
+							## constructor(props) {"{"}<br />
+							##     super(props);<br />
+							##     this.state = {"{"}<br />
+							##         sidebarOpen: false<br />
+							##     };<br />
+							##     this.handleToggleSidebar = this.handleToggleSidebar.bind(this);<br />
+							## }<br />
+							## handleToggleSidebar(open) {"{"}<br />
+							##     this.setState({"{"}<br />
+							##         sidebarOpen: open<br />
+							##     });<br />
+							## }
+							</code></pre>
+						</li>
+					</ol>
 				</div>
 			</div>
 		);
